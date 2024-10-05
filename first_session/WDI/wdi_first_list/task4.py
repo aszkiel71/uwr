36f8da43606ea2c29e1988b8e5d291dab788e4df
aszkiel71 <returnpll@gmail.com>
2024-10-05 15:20:06 +0200

    Another assigments

---
+++ first_session/WDI/wdi_first_list/task4.py
@@ -0,0 +1,8 @@
+x = int(input("Podaj wartosc x : "))
+y = int(input("Podaj wartosc y : "))
+
+tmp = x
+x = y
+y = tmp
+print(f'Po zamianie :\nx = {x}, y = {y} ')
+
---
+++ first_session/WDI/wdi_first_list/task5.py
@@ -0,0 +1,12 @@
+x = int(input("Podaj pierwsza liczbe : "))
+y = int(input("Podaj druga liczbe : "))
+wynik = 0
+
+if x > y:
+    bigger = x
+    smaller = y
+else:
+    bigger = y
+    smaller = x
+
+

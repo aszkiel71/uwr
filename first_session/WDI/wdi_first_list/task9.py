18380bb65fe404667c72d82dbdd538e3a4c1eefc
Wojtek <returnpll@gmail.com>
2024-10-10 10:31:46 +0200

    task9.py from first list

---
+++ first_session/WDI/wdi_first_list/task9.py
@@ -0,0 +1,18 @@
+S = ['a', 'b']
+T = ['a', 'b', 'c', 'd', 'a', 'b', 'a', 'D', 'a', 'b']
+m = 1 # 2
+n = 7 # 8
+tmp = 0
+counter = 0
+
+for i in range(n-m):
+    tmp_2 = i
+    for j in range(m):
+        if T[tmp_2] == S[j]:
+            tmp = 1
+        else:
+            tmp = 0
+    if tmp == 1:
+        counter += 1
+
+print(counter)

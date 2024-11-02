eff14ba07dbf5b45cdd1de7176c86317c1d5e618
Wojtek <returnpll@gmail.com>
2024-11-02 15:24:50 +0100

    some tasks

--- first_session/wdi/wdi_fourth_list/task3.py
+++ first_session/wdi/wdi_fourth_list/task3.py
@@ -1,19 +1,10 @@
 from task2 import nwd
 
 
-
 def max_factor(n, a):
-    result = 1
-    for i in range(n-1):
-        tmp = nwd(a[i], a[i+1])
-        if tmp > result:
-            for j in range(n):
-                if a[j] >= tmp and a[j] % tmp == 0:
-                    result = tmp
-                else:
-                    break
-
+    result = a[0]
+    for i in range(n):
+        result = nwd(a[i], result)
     return result
-
-
+print(max_factor(6, [25, 25, 125, 25, 10, 15]))
 

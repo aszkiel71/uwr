dfdf99f7ba02e6bea879d7d3926130187f66d770
Wojtek <returnpll@gmail.com>
2025-02-22 19:49:06 +0100

    algo_praktyczna_lista_0

--- [2025] Second Session/Algorytmika Praktyczna/list0/task4.cpp
+++ [2025] Second Session/Algorytmika Praktyczna/list0/task4.cpp
@@ -2,16 +2,47 @@
 
 using namespace std;
 
+void quick_sort(int *arr, int left, int right) {
+    if (left >= right)
+        return;
 
+    int i = left, j = right;
+    int pivot = arr[(left + right) / 2];
 
-int main() {
-    int n;
-    cin >> n;
-    int a[n];
-    while(n--) {
-      cin >> a[n];
+    while (i <= j) {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+        if (i <= j) {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
     }
 
+    if (left < j)
+        quick_sort(arr, left, j);
+    if (i < right)
+        quick_sort(arr, i, right);
+}
+
+int main() {
+    int *arr;
+    int n;
+    cin >> n;
+
+    arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quick_sort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
+
+    delete[] arr;
 
     return 0;
 }

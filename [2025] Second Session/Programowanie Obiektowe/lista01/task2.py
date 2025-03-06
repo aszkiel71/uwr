b40eb9024996d3f0999c536c759ce3ec08c9e019
aszkiel71 <353873@uwr.edu.pl>
2025-03-06 12:19:16 +0100

    oop

--- [2025] Second Session/Programowanie Obiektowe/lista01/task2.py
+++ [2025] Second Session/Programowanie Obiektowe/lista01/task2.py
@@ -29,32 +29,25 @@ def coprime_imp(n):
             res.append(k)
     return res
 
-def coprime_fun(n):
+def coprime_almost_fun(n):
     return [k for k in range(1, n+1) if gcd_fun(n, k) == 1]
+
+def coprime_fun_v2(n, k = 2, res = [1]):
+    if n == k:                  return set(res)
+    if gcd_fun(n, k) == 1:      return coprime_fun_v2(n, k+1, res + [k])
+    return coprime_fun_v2(n, k+1, res)
+
 
 # test cases:
 
 print(coprime_imp(10))
-print(coprime_fun(10))
+print(coprime_fun_v2(10))
 print(coprime_imp(29))
-print(coprime_fun(29))
+print(coprime_fun_v2(29))
 
 # nie wiem czy dobrze rozumiem polecenie wiec jakby co to ponizej inna wersja: wypisujemy wszystkie pary p, q <= n t.z. gcd(p, q) = 1
 # jezeli juz wyzej dobrze zoruzmiaome poelcenie to dalsza czesc mozna zignorowac, dodaje ją dla pewnosci
 
-def coprime_pairs_imp(n):
-    result = []
-    for p in range(1, n + 1):
-        for q in range(1, n + 1):
-            if gcd_imp(p, q) == 1:
-                result.append((p, q))
-    return result
-
-def coprime_pairs_fun(n):
-    return [(p, q) for p in range(1, n + 1) for q in range(1, n + 1) if gcd_fun(p, q) == 1]
-
-print(coprime_pairs_imp(10))
-print(coprime_pairs_fun(10))
 
 
 

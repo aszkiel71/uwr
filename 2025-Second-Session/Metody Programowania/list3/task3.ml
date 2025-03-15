3be7303a4f9258891fff54448a9c1beb99bb6ee7
aszkiel71 <353873@uwr.edu.pl>
2025-03-15 19:15:41 +0100

    MP list

--- 2025-Second-Session/Metody Programowania/list3/task3.ml
+++ 2025-Second-Session/Metody Programowania/list3/task3.ml
@@ -1,8 +1,10 @@
 let build_list n f =
-  let rec it x xs =
-    if x = n then xs
-    else it (x+1) (f x :: xs)
-  in it 1 [];;
+  let rec it n xs =
+  if n = (-1) then xs
+  else it (n-1) ((f n)::xs)
+  in it (n-1) [];;
+
+
 
 let add1 x = x + 1;; (*for tests*)
 
@@ -10,19 +12,29 @@ let negative n =
   if n < 0 then n
   else (n * -1);;
 
-let negatives n =
-  build_list n negative;;
 
-let rev n = 1.0 /. float_of_int n;;
+
+  (*******)
+let negative2 n = -n-1;;
+
+let negatives n =
+  build_list n negative2;;
+  (*******)
+
+
+let rev n = 1.0 /. float_of_int (n+1);;
 
 let reciprocals n = 
-  build_list (n+1) rev;;
+  build_list n rev;;
+
+
 
 let db n = 2*n;;
 
 let evens n =
-  0 :: build_list n db;;
+  build_list n db;;
 
+  
 let row i n =
   build_list n (fun j -> if i = j then 1 else 0);;
 

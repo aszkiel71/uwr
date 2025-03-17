1dacb31952da729835c0b8798694bcb9330a6306
Wojtek <142600878+aszkiel71@users.noreply.github.com>
2025-03-17 19:18:13 +0100

    task4.ml

--- 2025-Second-Session/Metody Programowania/list3/task4.ml
+++ 2025-Second-Session/Metody Programowania/list3/task4.ml
@@ -1,14 +1,27 @@
+let empty_set = fun _ -> false;;
+let singleton a = fun x -> x = a;;
+let in_set a s = s a;;
+let union a b = fun x -> a x || b x;;
+let intersect a b = fun x -> a x && b x;;
 
-type 'a set = 'a -> bool
+(*naturalne [1, 10]*)
+let numbers_1_to_10 = fun x -> x >= 1 && x <= 10;;
 
-let empty_set : 'a set = fun _ -> false
+let test1 = in_set 5 numbers_1_to_10;; 
+let test2 = in_set 0 numbers_1_to_10;;  
+let test3 = in_set 11 numbers_1_to_10;; 
 
-let singleton (a : 'a) : 'a set = fun x -> x = a
+let singleton_13 = singleton 13;;
+
+let numbers_1_to_10_and_13 = union numbers_1_to_10 singleton_13;;
+
+let even_numbers = fun x -> x mod 2 = 0;;
+
+let even_from_our_set = intersect numbers_1_to_10_and_13 even_numbers;;
 
 
-let in_set (a : 'a) (s : 'a set) : bool = s a
-
-
-let union (s : 'a set) (t : 'a set) : 'a set = fun x -> s x || t x
-
-let intersect (s : 'a set) (t : 'a set) : 'a set = fun x -> s x && t x
+let test1 = in_set 2 even_from_our_set;; 
+let test2 = in_set 3 even_from_our_set;; 
+let test3 = in_set 13 even_from_our_set;; 
+let test4 = in_set 10 even_from_our_set;;
+let test5 = in_set 12 even_from_our_set;;

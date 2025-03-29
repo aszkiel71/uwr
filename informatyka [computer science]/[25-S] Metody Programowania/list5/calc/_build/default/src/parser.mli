6e69f48f703a1c64ae7174e29a7a6d09c088fe2e
aszkiel71 <353873@uwr.edu.pl>
2025-03-29 19:54:20 +0100

    mp

---
+++ informatyka [computer science]/[25-S] Metody Programowania/list5/calc/_build/default/src/parser.mli
@@ -0,0 +1,21 @@
+
+(* The type of tokens. *)
+
+type token = 
+  | TIMES
+  | RPAREN
+  | PLUS
+  | MINUS
+  | LPAREN
+  | INT of (int)
+  | FLOAT of (float)
+  | EOF
+  | DIV
+
+(* This exception is raised by the monolithic API functions. *)
+
+exception Error
+
+(* The monolithic API. *)
+
+val main: (Lexing.lexbuf -> token) -> Lexing.lexbuf -> (Ast.expr)

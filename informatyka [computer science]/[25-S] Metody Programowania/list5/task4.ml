2bf0ec7a9675cd6131eccab931229f133d3f4a3f
aszkiel71 <353873@uwr.edu.pl>
2025-03-31 02:04:10 +0200

    mp

--- informatyka [computer science]/[25-S] Metody Programowania/list5/task4.ml
+++ informatyka [computer science]/[25-S] Metody Programowania/list5/task4.ml
@@ -3,7 +3,7 @@ let rec parens_ok str =
     let rec it arr_str b1 b2 b3 =
       match arr_str with
       | [] -> if b1 <> 0 || b2 <> 0 || b3 <> 0 then false else true
-      | x :: xs -> if b1 < 0 || b2 < 0 || b3 < 0 then false else
+      | x :: xs -> if b1 < 0 || b2 < 0 || b3 < 0 || (x <> '(' && x <> ')' && x <> '{' && x <> '}' && x <> '[' && x <> ']') then false else
                               if x = '(' then it xs (b1+1) b2 b3
                               else if x = ')' then it xs (b1-1) b2 b3
                               else if x = '[' then it xs b1 (b2+1) b3

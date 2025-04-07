23617d6304e301420fd89ad6b19b14a69b7ef457
aszkiel71 <353873@uwr.edu.pl>
2025-04-07 19:53:03 +0200

    ap-list7

---
+++ informatyka [computer science]/[25-S] Algorytmika Praktyczna/list7/task1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int find(int ST[], int v, int S, int node = 0) {
+    if (node >= S) return node;
+    if (ST[2 * node + 1] >= v) return find(ST, v, S, 2 * node + 1);
+    else return find(ST, v, S, 2 * node + 2);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int N, Q; cin >> N >> Q;
+    int S = 1; while (S < N) S *= 2; S--;
+    int Hotel[4 * S + 4] = {0};
+    for (int i = S; i < S + N; ++i) cin >> Hotel[i];
+    for (int i = S - 1; i >= 0; --i) Hotel[i] = max(Hotel[2*i + 1], Hotel[2*i + 2]);
+    while (Q--) {
+        int q; cin >> q;
+        if (q > Hotel[0]) {cout << 0 << ' ';    continue;
+        }
+        int idx = find(Hotel, q, S);
+        cout << (idx - S + 1) << ' ';
+        Hotel[idx] -= q;
+        while (idx > 0) {
+            idx = (idx - 1) / 2;
+            Hotel[idx] = max(Hotel[2*idx + 1], Hotel[2*idx + 2]);
+        }
+    }
+    return 0;
+}
---
+++ informatyka [computer science]/[25-S] Algorytmika Praktyczna/list7/task2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int find(int ST[], int v, int S, int node = 0){
+    if(node >= S) return node;
+    if(ST[2*node + 1] >= v) return find(ST, v, S, 2*node + 1);
+    else return find(ST, v - ST[2*node + 1], S, 2*node + 2);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int N; cin>>N;
+    int S = 1; while(S < N) S *= 2; S--;
+    int ST[4*N + 4] = {0};
+    int Arr[N]; for(int i = 0; i < N; i++) cin >> Arr[i];
+    for(int i = S; i < S + N; i++) ST[i] = 1;
+    for(int i = S - 1; i >= 0; i--) ST[i] = ST[2*i + 1] + ST[2*i + 2];
+    int Q = N;
+    while(Q--){
+        int q; cin >> q;
+        int idx = find(ST, q, S);
+        cout << Arr[idx - S] << ' ';
+        ST[idx] = 0;
+        int node = idx;
+        while(node > 0){
+            node = (node - 1)/2;
+            ST[node] = ST[2*node + 1] + ST[2*node + 2];
+        }
+    }
+    return 0;
+}

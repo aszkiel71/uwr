3df75b4d22dd98dbfd158ac30ec28f889a327907
aszkiel71 <353873@uwr.edu.pl>
2025-04-18 10:56:34 +0200

    py

--- informatyka [computer science]/[24-W] Wstep do pythona/eleventh_list/wprawka.py
+++ informatyka [computer science]/[24-W] Wstep do pythona/eleventh_list/wprawka.py
@@ -1,6 +1,6 @@
-def hanoi(n, skad, dokad):
+def hanoi(n, skad = 1, dokad = 3):
     return hanoi(n-1, skad, 6-(skad+dokad)) + (f"{skad} -> {dokad}, ") + hanoi(n-1, 6-(skad+dokad), dokad) if n else ""
-print(hanoi(3, 1, 3))
+print(hanoi(5))
 
 
 # n-1 krazkow z skad na trzeci krazek skad

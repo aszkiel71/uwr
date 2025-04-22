e8be6dd57853691e0885cea734a1ec16fb032e0e
aszkiel71 <353873@uwr.edu.pl>
2025-04-22 14:27:16 +0200

    ap XDD

---
+++ informatyka [computer science]/[25-S] Algorytmika Praktyczna/list8/task7.cpp
@@ -0,0 +1,2 @@
+#include <iostream> // XD
+int main() {  using namespace std; ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);    int N, Q; cin >> N >> Q;   while(Q--) {cout << 1 << '\n';}   return 0;}

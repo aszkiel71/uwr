710b0517d27eebf481a3a94dee36e83ea5c0815a
aszkiel71 <returnpll@gmail.com>
2025-05-21 16:50:13 +0200

    mp

---
+++ informatyka [computer science]/[25-S] Metody Programowania/list9/fun_exc/_build/default/src/ast.ml
@@ -0,0 +1,38 @@
+type bop =
+  (* arithmetic *)
+  | Add | Sub | Mult | Div
+  (* logic *)
+  | And | Or
+  (* comparison *)
+  | Eq | Neq | Leq | Lt | Geq | Gt
+
+type ident = string
+
+type expr = 
+  | Int    of int
+  | Binop  of bop * expr * expr
+  | Bool   of bool
+  | If     of expr * expr * expr
+  | Let    of ident * expr * expr
+  | Var    of ident
+  | Unit
+  | Pair   of expr * expr
+  | Fst    of expr
+  | Snd    of expr
+  | Match  of expr * ident * ident * expr
+  | IsPair of expr
+  | Fun    of ident * expr
+  | Funrec of ident * ident * expr
+  | App    of expr * expr
+  | Throw
+  | Try    of expr * expr
+  (* zad 1 *)
+  | While of expr * expr
+  | Ref of expr
+  | Deref of expr
+  | Assign of expr * expr
+  | Seq of expr * expr
+  (* zad2 *)
+  | Break
+  | Continue
+
